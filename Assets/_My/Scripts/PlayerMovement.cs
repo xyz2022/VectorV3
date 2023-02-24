404e8906b6e48ec52ababa6c20f0c051aa2d47e0
xyz2022 <91395091+xyz2022@users.noreply.github.com>
2023-02-24 19:44:56 +1100

    Fixed right hand rotation

--- Assets/_My/Scripts/PlayerMovement.cs
+++ Assets/_My/Scripts/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
     public Transform transformForForward;
     public float walkSpeed = 20.0f;
     public float rotateSpeed = 80.0f;
+    public float xThreshold = 0.1f;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,10 +20,12 @@ public class PlayerMovement : MonoBehaviour
         
         Vector2 v2 = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick, OVRInput.Controller.LTouch);
         float y = transform.position.y;
-        transform.Translate((transformForForward.forward * v2.y * walkSpeed * Time.deltaTime));
-        transform.Translate((transformForForward.right * v2.x * walkSpeed * Time.deltaTime));
-        transform.Translate(new Vector3(0, y - transform.position.y, 0));
-
+        if (v2.x > xThreshold || v2.x < -xThreshold)
+        {
+            transform.Translate((transformForForward.forward * v2.y * walkSpeed * Time.deltaTime));
+            transform.Translate((transformForForward.right * v2.x * walkSpeed * Time.deltaTime));
+            transform.Translate(new Vector3(0, y - transform.position.y, 0));
+        }
         float v1 = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, OVRInput.Controller.LTouch);
         transform.Translate(new Vector3(0, 0 + (v1 * walkSpeed * Time.deltaTime), 0));
         v1 = OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, OVRInput.Controller.LTouch);
